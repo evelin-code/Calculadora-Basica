26f9adeee75c88f960afd23c93f7d00828694d07
evelin0521 <f.gongoraandrade@gmail.com>
2021-12-07 13:47:37 -0500

    Actualización del código

--- InterfazCalculadora.py
+++ InterfazCalculadora.py
@@ -1,13 +1,16 @@
-
 from tkinter import *
 
 root = Tk()
 root.title("Calculadora")
 root.resizable(0,0)
-root.geometry("459x448+750+300")
+root.geometry("459x458+750+300")
 root.config(background='#7A89C9')
 var1 = Entry(root)
 var1.grid(row=1, columnspan=6, sticky=W+E)
+var1.config(font=("Calibri 16"))
+
+def __init__(self):
+        pass
 
 #Función para obtener el número del botón
 i = 0
@@ -16,7 +19,33 @@ def obtenerNumeros(n):
     var1.insert(i, n)
     i+=1
 
-# Botones de los números
+#Función para obtener el operador
+def obtenerOperadores(operador):
+    global i
+    var1.insert(i, operador)
+    i+=1
+
+#Limpiar la pantalla
+def limpiar():
+    var1.delete(0, END)
+
+#Limpiar de uno en uno los elementos
+def undo():
+    var1_state = var1.get()
+    if len(var1_state):
+        var1_new_state = var1_state[:-1]
+        limpiar()
+        var1.insert(0, var1_new_state)
+    else:
+        limpiar()
+        var1.insert(0, 'Error')
+
+#Símbolo del igual
+def calcular():
+    pass
+
+# Botones de los números y signos
+
 Button(root, text="1", width=15, height=5, command=lambda:obtenerNumeros(1)).grid(row=2, column=0, sticky=W+E)
 Button(root, text="2", width=15, height=5, command=lambda:obtenerNumeros(2)).grid(row=2, column=1, sticky=W+E)
 Button(root, text="3", width=15, height=5, command=lambda:obtenerNumeros(3)).grid(row=2, column=2, sticky=W+E)
@@ -29,14 +58,14 @@ Button(root, text="7", width=15, height=5, command=lambda:obtenerNumeros(7)).gri
 Button(root, text="8", width=15, height=5, command=lambda:obtenerNumeros(8)).grid(row=4, column=1, sticky=W+E)
 Button(root, text="9", width=15, height=5, command=lambda:obtenerNumeros(9)).grid(row=4, column=2, sticky=W+E)
 
-Button(root, text="AC", width=15, height=5).grid(row=5, column=0, sticky=W+E)
+Button(root, text="AC", width=15, height=5, command=lambda:limpiar()).grid(row=5, column=0, sticky=W+E)
 Button(root, text="0", width=15, height=5, command=lambda:obtenerNumeros(0)).grid(row=5, column=1, sticky=W+E)
-Button(root, text="←", width=15, height=5).grid(row=5, column=2, sticky=W+E)
+Button(root, text="←", width=15, height=5, command=lambda:undo()).grid(row=5, column=2, sticky=W+E)
 
-Button(root, text="+", width=15, height=5).grid(row=2, column=3, sticky=W+E)
-Button(root, text="-", width=15, height=5).grid(row=3, column=3, sticky=W+E)
-Button(root, text="/", width=15, height=5).grid(row=4, column=3, sticky=W+E)
-Button(root, text="*", width=15, height=5).grid(row=5, column=3, sticky=W+E)
+Button(root, text="+", width=15, height=5, command=lambda:obtenerOperadores("+")).grid(row=2, column=3, sticky=W+E)
+Button(root, text="-", width=15, height=5, command=lambda:obtenerOperadores("-")).grid(row=3, column=3, sticky=W+E)
+Button(root, text="/", width=15, height=5, command=lambda:obtenerOperadores("/")).grid(row=4, column=3, sticky=W+E)
+Button(root, text="*", width=15, height=5, command=lambda:obtenerOperadores("*")).grid(row=5, column=3, sticky=W+E)
 
 Button(root, text="=", width=15, height=5).grid(row=6, columnspan=6, sticky=W+E)
 
