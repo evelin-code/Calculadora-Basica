b55fb4c6f7a63ee2a0b1911b0e47386f19cb3723
evelin0521 <f.gongoraandrade@gmail.com>
2021-12-07 14:43:37 -0500

    Actualización del proyecto

--- InterfazCalculadora.py
+++ InterfazCalculadora.py
@@ -10,60 +10,32 @@ var1 = Entry(root)
 var1.grid(row=1, columnspan=6, sticky=W+E)
 var1.config(font=("Calibri 16"))
 
-""" #Función para obtener el número del botón
-i = 0
-def obtenerNumeros(n):
-    global i
-    var1.insert(i, n)
-    i+=1
+#Instancia de la clase
 
-#Función para obtener el operador
-def obtenerOperadores(operador):
-    global i
-    var1.insert(i, operador)
-    i+=1
-
-#Limpiar la pantalla
-def limpiar():
-    var1.delete(0, END)
-
-#Limpiar de uno en uno los elementos
-def undo():
-    var1_state = var1.get()
-    if len(var1_state):
-        var1_new_state = var1_state[:-1]
-        limpiar()
-        var1.insert(0, var1_new_state)
-    else:
-        limpiar()
-        var1.insert(0, 'Error') """
-
-#Símbolo del igual
-def calcular():
-    pass
+funciones= Funciones(var1)
 
 # Botones de los números y signos
 
-Button(root, text="1", width=15, height=5, command=lambda:obtenerNumeros(1)).grid(row=2, column=0, sticky=W+E)
-Button(root, text="2", width=15, height=5, command=lambda:obtenerNumeros(2)).grid(row=2, column=1, sticky=W+E)
-Button(root, text="3", width=15, height=5, command=lambda:obtenerNumeros(3)).grid(row=2, column=2, sticky=W+E)
+Button(root, text="1", width=15, height=5, command=lambda:funciones.obtenerNumeros(1)).grid(row=2, column=0, sticky=W+E)
+Button(root, text="2", width=15, height=5, command=lambda:funciones.obtenerNumeros(2)).grid(row=2, column=1, sticky=W+E)
+Button(root, text="3", width=15, height=5, command=lambda:funciones.obtenerNumeros(3)).grid(row=2, column=2, sticky=W+E)
 
-Button(root, text="4", width=15, height=5, command=lambda:obtenerNumeros(4)).grid(row=3, column=0, sticky=W+E)
-Button(root, text="5", width=15, height=5, command=lambda:obtenerNumeros(5)).grid(row=3, column=1, sticky=W+E)
-Button(root, text="6", width=15, height=5, command=lambda:obtenerNumeros(6)).grid(row=3, column=2, sticky=W+E)
+Button(root, text="4", width=15, height=5, command=lambda:funciones.obtenerNumeros(4)).grid(row=3, column=0, sticky=W+E)
+Button(root, text="5", width=15, height=5, command=lambda:funciones.obtenerNumeros(5)).grid(row=3, column=1, sticky=W+E)
+Button(root, text="6", width=15, height=5, command=lambda:funciones.obtenerNumeros(6)).grid(row=3, column=2, sticky=W+E)
 
-Button(root, text="7", width=15, height=5, command=lambda:obtenerNumeros(7)).grid(row=4, column=0, sticky=W+E)
-Button(root, text="8", width=15, height=5, command=lambda:obtenerNumeros(8)).grid(row=4, column=1, sticky=W+E)
-Button(root, text="9", width=15, height=5, command=lambda:obtenerNumeros(9)).grid(row=4, column=2, sticky=W+E)
+Button(root, text="7", width=15, height=5, command=lambda:funciones.obtenerNumeros(7)).grid(row=4, column=0, sticky=W+E)
+Button(root, text="8", width=15, height=5, command=lambda:funciones.obtenerNumeros(8)).grid(row=4, column=1, sticky=W+E)
+Button(root, text="9", width=15, height=5, command=lambda:funciones.obtenerNumeros(9)).grid(row=4, column=2, sticky=W+E)
 
-Button(root, text="AC", width=15, height=5, command=lambda:limpiar()).grid(row=5, column=0, sticky=W+E)
-Button(root, text="0", width=15, height=5, command=lambda:obtenerNumeros(0)).grid(row=5, column=1, sticky=W+E)
-Button(root, text="←", width=15, height=5, command=lambda:undo()).grid(row=5, column=2, sticky=W+E)
+Button(root, text="AC", width=15, height=5, command=lambda:funciones.limpiar()).grid(row=5, column=0, sticky=W+E)
+Button(root, text="0", width=15, height=5, command=lambda:funciones.obtenerNumeros(0)).grid(row=5, column=1, sticky=W+E)
+Button(root, text="←", width=15, height=5, command=lambda:funciones.undo()).grid(row=5, column=2, sticky=W+E)
 
-Button(root, text="+", width=15, height=5, command=lambda:obtenerOperadores("+")).grid(row=2, column=3, sticky=W+E)
-Button(root, text="-", width=15, height=5, command=lambda:obtenerOperadores("-")).grid(row=3, column=3, sticky=W+E)
-Button(root, text="/", width=15, height=5, command=lambda:obtenerOperadores("/")).grid(row=4, column=3, sticky=W+E)
-Button(root, text="*", width=15, height=5, command=lambda:obtenerOperadores("*")).grid(row=5, column=3, sticky=W+E)
+Button(root, text="+", width=15, height=5, command=lambda:funciones.obtenerOperadores("+")).grid(row=2, column=3, sticky=W+E)
+Button(root, text="-", width=15, height=5, command=lambda:funciones.obtenerOperadores("-")).grid(row=3, column=3, sticky=W+E)
+Button(root, text="/", width=15, height=5, command=lambda:funciones.obtenerOperadores("/")).grid(row=4, column=3, sticky=W+E)
+Button(root, text="*", width=15, height=5, command=lambda:funciones.obtenerOperadores("*")).grid(row=5, column=3, sticky=W+E)
 
 Button(root, text="=", width=15, height=5).grid(row=6, columnspan=6, sticky=W+E)
 
--- claseFunciones.py
+++ claseFunciones.py
@@ -1,54 +1,55 @@
 #from InterfazCalculadora import *
-
+from tkinter import *
 class Funciones:
 
-    def __init__(self):
-        pass
+    def __init__(self, var1):
+        self.var1 = var1
+        print(self.var1)
 
     #Función para obtener el número del botón
     i = 0
-    def obtenerNumeros(n):
+    def obtenerNumeros(self, n):
         global i
-        var1.insert(i, n)
-        i+=1
+        self.var1.insert(self.i, n)
+        self.i+=1
 
     #Función para obtener el operador
-    def obtenerOperadores(operador):
+    def obtenerOperadores(self, operador):
         global i
-        var1.insert(i, operador)
-        i+=1
+        self.var1.insert(self.i, operador)
+        self.i+=1
 
     #Limpiar la pantalla
-    def limpiar():
-        var1.delete(0, END)
+    def limpiar(self):
+        self.var1.delete(0, END)
 
     #Limpiar de uno en uno los elementos
-    def undo():
-        var1_state = var1.get()
-        if len(var1_state):
-            var1_new_state = var1_state[:-1]
-            limpiar()
-            var1.insert(0, var1_new_state)
+    def undo(self):
+        self.var1_state = self.var1.get()
+        if len(self.var1_state):
+            var1_new_state = self.var1_state[:-1]
+            self.limpiar()
+            self.var1.insert(0, var1_new_state)
         else:
-            limpiar()
-            var1.insert(0, 'Error')
+            self.limpiar()
+            self.var1.insert(0, 'Error')
 
     #Símbolo del igual
-    def calcular():
+    def calcular(self):
         pass
 
     #Suma
-    def SumaP(numero1, numero2):
+    def SumaP(self, numero1, numero2):
         sumaP = numero1 + numero2
         return sumaP
 
     #Resta
-    def RestaP(numero1, numero2):
+    def RestaP(self, numero1, numero2):
         restaP = numero1 - numero2
         return restaP
 
     #Division
-    def DivisionP(numero1, numero2):
+    def DivisionP(self, numero1, numero2):
         try:
             divisionP = numero1 / numero2
             print("La division es ",divisionP)
@@ -56,6 +57,6 @@ class Funciones:
             print("Error! ZeroDivisionError")
 
     #Multiplicación
-    def MultiplicacionP(numero1, numero2):
+    def MultiplicacionP(self, numero1, numero2):
         multiP = numero1 * numero2
         print("La multiplicacion es ",multiP)
