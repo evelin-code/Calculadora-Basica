89e6ae3c4bd6c1b3c64ab5841eef9d9f8c83599d
evelin0521 <f.gongoraandrade@gmail.com>
2021-12-08 18:34:51 -0500

    Primera parte de la depuración

--- InterfazCalculadora.py
+++ InterfazCalculadora.py
@@ -4,37 +4,37 @@ from tkinter import *
 root = Tk()
 root.title("Calculadora")
 root.resizable(0,0)
-root.geometry("459x458+750+300")
+root.geometry("459x458+700+100")
 root.config(background='#7A89C9')
 var1 = Entry(root)
 var1.grid(row=1, columnspan=6, sticky=W+E)
 var1.config(font=("Calibri 16"))
 
-funciones= Funciones(var1) #Instancia de la clase
+funciones= Funciones(var1) # Instancia de la clase
 
-#Botones de los números y signos
+# Botones de los números y signos
 
-Button(root, text="1", width=15, height=5, command=lambda:funciones.obtenerNumeros("1")).grid(row=2, column=0, sticky=W+E)
-Button(root, text="2", width=15, height=5, command=lambda:funciones.obtenerNumeros("2")).grid(row=2, column=1, sticky=W+E)
-Button(root, text="3", width=15, height=5, command=lambda:funciones.obtenerNumeros("3")).grid(row=2, column=2, sticky=W+E)
+Button(root, text="1", width=15, height=5, command=lambda:funciones.operacion("1")).grid(row=2, column=0, sticky=W+E)
+Button(root, text="2", width=15, height=5, command=lambda:funciones.operacion("2")).grid(row=2, column=1, sticky=W+E)
+Button(root, text="3", width=15, height=5, command=lambda:funciones.operacion("3")).grid(row=2, column=2, sticky=W+E)
 
-Button(root, text="4", width=15, height=5, command=lambda:funciones.obtenerNumeros("4")).grid(row=3, column=0, sticky=W+E)
-Button(root, text="5", width=15, height=5, command=lambda:funciones.obtenerNumeros("5")).grid(row=3, column=1, sticky=W+E)
-Button(root, text="6", width=15, height=5, command=lambda:funciones.obtenerNumeros("6")).grid(row=3, column=2, sticky=W+E)
+Button(root, text="4", width=15, height=5, command=lambda:funciones.operacion("4")).grid(row=3, column=0, sticky=W+E)
+Button(root, text="5", width=15, height=5, command=lambda:funciones.operacion("5")).grid(row=3, column=1, sticky=W+E)
+Button(root, text="6", width=15, height=5, command=lambda:funciones.operacion("6")).grid(row=3, column=2, sticky=W+E)
 
-Button(root, text="7", width=15, height=5, command=lambda:funciones.obtenerNumeros("7")).grid(row=4, column=0, sticky=W+E)
-Button(root, text="8", width=15, height=5, command=lambda:funciones.obtenerNumeros("8")).grid(row=4, column=1, sticky=W+E)
-Button(root, text="9", width=15, height=5, command=lambda:funciones.obtenerNumeros("9")).grid(row=4, column=2, sticky=W+E)
+Button(root, text="7", width=15, height=5, command=lambda:funciones.operacion("7")).grid(row=4, column=0, sticky=W+E)
+Button(root, text="8", width=15, height=5, command=lambda:funciones.operacion("8")).grid(row=4, column=1, sticky=W+E)
+Button(root, text="9", width=15, height=5, command=lambda:funciones.operacion("9")).grid(row=4, column=2, sticky=W+E)
 
 Button(root, text="AC", width=15, height=5, command=lambda:funciones.limpiar()).grid(row=5, column=0, sticky=W+E)
-Button(root, text="0", width=15, height=5, command=lambda:funciones.obtenerNumeros("0")).grid(row=5, column=1, sticky=W+E)
+Button(root, text="0", width=15, height=5, command=lambda:funciones.operacion("0")).grid(row=5, column=1, sticky=W+E)
 Button(root, text="←", width=15, height=5, command=lambda:funciones.undo()).grid(row=5, column=2, sticky=W+E)
 
-Button(root, text="+", width=15, height=5, command=lambda:funciones.obtenerOperadores("+")).grid(row=2, column=3, sticky=W+E)
-Button(root, text="-", width=15, height=5, command=lambda:funciones.obtenerOperadores("-")).grid(row=3, column=3, sticky=W+E)
-Button(root, text="/", width=15, height=5, command=lambda:funciones.obtenerOperadores("/")).grid(row=4, column=3, sticky=W+E)
-Button(root, text="*", width=15, height=5, command=lambda:funciones.obtenerOperadores("*")).grid(row=5, column=3, sticky=W+E)
+Button(root, text="+", width=15, height=5, command=lambda:funciones.operacion("+")).grid(row=2, column=3, sticky=W+E)
+Button(root, text="-", width=15, height=5, command=lambda:funciones.operacion("-")).grid(row=3, column=3, sticky=W+E)
+Button(root, text="/", width=15, height=5, command=lambda:funciones.operacion("/")).grid(row=4, column=3, sticky=W+E)
+Button(root, text="*", width=15, height=5, command=lambda:funciones.operacion("*")).grid(row=5, column=3, sticky=W+E)
 
-Button(root, text="=", width=15, height=5).grid(row=6, columnspan=6, sticky=W+E)
+Button(root, text="=", width=15, height=5, command=lambda:funciones.operacion("=")).grid(row=6, columnspan=6, sticky=W+E)
 
 root.mainloop()
--- claseFunciones.py
+++ claseFunciones.py
@@ -1,4 +1,5 @@
 from tkinter import *
+
 class Funciones: 
 
     operaciones = []
@@ -8,61 +9,9 @@ class Funciones:
     def __init__(self, var1):
         self.var1 = var1
 
-    def operar(self, n):
-        sn = self.mostrar(n)
-        result = 0
 
-        if sn:
-            print(self.operaciones)
-
-            if(len(self.operaciones) > 2):
-                if self.operaciones.__contains__("="):
-                    del self.operaciones[self.operaciones.index("=")]
-                    for i in self.operaciones:
-                        # ["5", "+", "2", "*", "3", "*", "4"] = 29
-                        # ["10", "/", "2", "+", "4"] =  9                 
-                        if self.operaciones.__contains__("*"):
-                            result = int(self.operaciones.__getitem__(self.operaciones.index("*")-1)) * int(self.operaciones.__getitem__(self.operaciones.index("*")+1))
-                            index = self.operaciones.index("*")
-                            self.operaciones[index] = result
-                            del self.operaciones[index-1]
-                            del self.operaciones[index]
-                            self.limpiar()
-                            self.var1.insert(0, result)
-                        elif self.operaciones.__contains__("/"):
-                            try:
-                                result = int(self.operaciones.__getitem__(self.operaciones.index("/")-1)) / int(self.operaciones.__getitem__(self.operaciones.index("/")+1))
-                                index = self.operaciones.index("/")
-                                self.operaciones[index] = result
-                                del self.operaciones[index-1]
-                                del self.operaciones[index]
-                                self.limpiar()
-                                self.var1.insert(0, result)
-                            except:
-                                self.limpiar()
-                                self.var1.insert(0, "No me exija dividir por 0")
-                                break
-                        elif self.operaciones.__contains__("+"): 
-                            result = int(self.operaciones.__getitem__(self.operaciones.index("+")-1)) + int(self.operaciones.__getitem__(self.operaciones.index("+")+1))
-                            index = self.operaciones.index("+")
-                            self.operaciones[index] = result
-                            del self.operaciones[index-1]
-                            del self.operaciones[index]
-                            self.limpiar()
-                            self.var1.insert(0, result)
-                        elif self.operaciones.__contains__("-"):
-                            result = int(self.operaciones.__getitem__(self.operaciones.index("-")-1)) - int(self.operaciones.__getitem__(self.operaciones.index("-")+1))
-                            index = self.operaciones.index("-")
-                            self.operaciones[index] = result
-                            del self.operaciones[index-1]
-                            del self.operaciones[index]
-                            self.limpiar()
-                            self.var1.insert(0, result)
-            
-            print(self.operaciones)
-    
-    def mostrar(self, n):
-        self.var1.insert(self.i, n)
+    def validaOperaciones(self, n):
+        self.mostrar(self.i, n)
         self.i += 1
 
         if not(n == "*" or n == "/" or n == "+" or n == "-" or n == "="):
@@ -74,34 +23,12 @@ class Funciones:
             self.operaciones.append(n)
             self.numero = ""
             return True
-            
-    #Función para obtener el número del botón
-    def obtenerNumeros(self, n):
-        global i
-        self.var1.insert(self.i, n)
-        self.numero += n
-        self.operaciones.append(self.numero)
-        self.i+=1
 
-    #Función para obtener el operador
-    def obtenerOperadores(self, operador):
-        global i
-        # if operador == "+":
-        #     self.limpiar()
-        #     self.var1.insert(0, self.sumaP(self.numero))
-        #     self.numero=""
-        # elif operador == "-":
-        #     self.limpiar()
-        #     self.var1.insert(0, self.restaP(self.numero))
-        #     self.numero=""
-
-        self.var1.insert(self.i, operador)
-        self.operaciones.append(operador)
-        self.i+=1
 
     #Limpiar la pantalla
     def limpiar(self):
         self.var1.delete(0, END)
+
 
     #Limpiar de uno en uno los elementos
     def undo(self):
@@ -113,3 +40,64 @@ class Funciones:
         else:
             self.limpiar()
             self.var1.insert(0, 'Error')
+
+
+    def mostrar(self, caracter, valor):
+        self.var1.insert(caracter, valor)
+
+    
+    def calcula(self, num1, num2, operador):
+        result = 0
+        index = self.operaciones.index(operador)
+
+        if operador == "*":
+            result = num1 * num2
+        elif operador == "/":
+            result = num1 / num2
+        elif operador == "+":
+            result = num1 + num2
+        else:
+            result = num1 - num2
+        
+        self.operaciones[index] = result
+        del self.operaciones[index-1]
+        del self.operaciones[index]
+        self.limpiar()
+        self.mostrar(0, result)
+
+    
+    def obtenerNumeros(self, operador, opcion):
+        if opcion == 1:
+            return int(self.operaciones.__getitem__(self.operaciones.index(operador)-1))
+        else:
+            return int(self.operaciones.__getitem__(self.operaciones.index(operador)+1))
+
+
+    def operacion(self, n):
+        sn = self.validaOperaciones(n)
+        
+        if sn:
+            if self.operaciones.__contains__("="):
+                del self.operaciones[self.operaciones.index("=")]
+                for i in self.operaciones:              
+                    if self.operaciones.__contains__("*"):
+                        num1 = self.obtenerNumeros("*", 1)
+                        num2 = self.obtenerNumeros("*", 2)
+                        self.calcula(num1, num2, "*")
+                    elif self.operaciones.__contains__("/"):
+                        try:
+                            num1 = self.obtenerNumeros("/", 1)
+                            num2 = self.obtenerNumeros("/", 2)
+                            self.calcula(num1, num2, "/")
+                        except:
+                            self.limpiar()
+                            self.mostrar(0, "No me exija dividir por 0")
+                            break
+                    elif self.operaciones.__contains__("+"): 
+                        num1 = self.obtenerNumeros("+", 1)
+                        num2 = self.obtenerNumeros("+", 2)
+                        self.calcula(num1, num2, "+")
+                    elif self.operaciones.__contains__("-"):
+                        num1 = self.obtenerNumeros("-", 1)
+                        num2 = self.obtenerNumeros("-", 2)
+                        self.calcula(num1, num2, "-")
