4206fb31d212a608bbf8add49c725929b4c924ba
evelin0521 <f.gongoraandrade@gmail.com>
2021-12-08 17:19:48 -0500

    Versión con suma, multi y divi

--- claseFunciones.py
+++ claseFunciones.py
@@ -1,35 +1,100 @@
 from tkinter import *
-class Funciones:
+class Funciones: 
 
+    operaciones = []
     numero = ""
     i = 0
 
     def __init__(self, var1):
         self.var1 = var1
-        print(self.var1)
 
+    def operar(self, n):
+        sn = self.mostrar(n)
+        result = 0
+
+        if sn:
+            #print(self.operaciones)
+
+            if(len(self.operaciones) > 2):
+                if self.operaciones.__contains__("="):
+                    del self.operaciones[self.operaciones.index("=")]
+                    for i in self.operaciones:
+                        # ["5", "+", "2", "*", "3", "*", "4"] = 29
+                        # ["10", "/", "2", "+", "4"] =  9                 
+                        if self.operaciones.__contains__("*"):
+                            result = int(self.operaciones.__getitem__(self.operaciones.index("*")-1)) * int(self.operaciones.__getitem__(self.operaciones.index("*")+1))
+                            index = self.operaciones.index("*")
+                            self.operaciones[index] = result
+                            del self.operaciones[index-1]
+                            del self.operaciones[index]
+                            self.limpiar()
+                            self.var1.insert(0, result)
+                        elif self.operaciones.__contains__("/"):
+                            try:
+                                result = int(self.operaciones.__getitem__(self.operaciones.index("/")-1)) / int(self.operaciones.__getitem__(self.operaciones.index("/")+1))
+                                index = self.operaciones.index("/")
+                                self.operaciones[index] = result
+                                del self.operaciones[index-1]
+                                del self.operaciones[index]
+                                self.limpiar()
+                                self.var1.insert(0, result)
+                            except:
+                                self.limpiar()
+                                self.var1.insert(0, "No me exija dividir por 0")
+                                break
+                        elif self.operaciones.__contains__("+"): 
+                            result = int(self.operaciones.__getitem__(self.operaciones.index("+")-1)) + int(self.operaciones.__getitem__(self.operaciones.index("+")+1))
+                            index = self.operaciones.index("+")
+                            self.operaciones[index] = result
+                            del self.operaciones[index-1]
+                            del self.operaciones[index]
+                            self.limpiar()
+                            self.var1.insert(0, result)
+                        elif self.operaciones.__contains__("-"):
+                            result = int(self.operaciones.__getitem__(self.operaciones.index("-")-1)) - int(self.operaciones.__getitem__(self.operaciones.index("-")+1))
+                            index = self.operaciones.index("-")
+                            self.operaciones[index] = result
+                            del self.operaciones[index-1]
+                            del self.operaciones[index]
+                            self.limpiar()
+                            self.var1.insert(0, result)
+
+    
+    def mostrar(self, n):
+        self.var1.insert(self.i, n)
+        self.i += 1
+
+        if not(n == "*" or n == "/" or n == "+" or n == "-" or n == "="):
+            self.numero += n
+            return False
+        else:
+            self.operaciones.append(self.numero)
+            self.operaciones.append(n)
+            self.numero = ""
+            return True
+            
     #Función para obtener el número del botón
     def obtenerNumeros(self, n):
         global i
         self.var1.insert(self.i, n)
-        self.numero+=n
+        self.numero += n
+        self.operaciones.append(self.numero)
         self.i+=1
 
     #Función para obtener el operador
     def obtenerOperadores(self, operador):
         global i
-        if operador == "+":
-            self.limpiar()
-            self.var1.insert(0, self.sumaP(self.numero))
-            self.numero=""
-        elif operador == "-":
-            self.limpiar()
-            #print(type(self.numero))
-            self.var1.insert(0, self.restaP(self.numero))
-            self.numero=""
-            print("hola")
+        # if operador == "+":
+        #     self.limpiar()
+        #     self.var1.insert(0, self.sumaP(self.numero))
+        #     self.numero=""
+        # elif operador == "-":
+        #     self.limpiar()
+        #     self.var1.insert(0, self.restaP(self.numero))
+        #     self.numero=""
 
         self.var1.insert(self.i, operador)
+        self.operaciones.append(operador)
         self.i+=1
 
     #Limpiar la pantalla
@@ -60,7 +125,7 @@ class Funciones:
     #Resta
     resta = 0
     def restaP(self, numero1):
-        self.resta -= int(numero1)
+        self.resta = int(numero1) - self.resta
         return self.resta
 
     #Division
